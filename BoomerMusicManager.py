97e80b619b2a8b6580632e9ab27ba57646ef00b2
NestorJankovic <nestor.jankovic@gmail.com>
2020-02-06 18:09:28 +0100

    Update BoomerMusicManager.py
    Fixed string sanitization function

--- BoomerMusicManager.py
+++ BoomerMusicManager.py
@@ -1,28 +1,29 @@
 import os
 import eyed3
 import shutil
+import unidecode
 from datetime import datetime
 from datetime import date
 
 fulllist=[]
 
 # The initial and target directory
-MediaHuman = 'C:\\Users\Ja\Downloads\MediaHuman\Music'
-Music = 'C:\\Users\Ja\Music'
+MediaHuman = 'C:\\Users\\Ja\\Downloads\\MediaHuman\\Music'
+Music = 'C:\\Users\\Ja\\Music'
 
 # Builds a list of new and existing media
 listMH = os.listdir(MediaHuman)
 listMU = os.listdir(Music)
 print('List of new tracks:')
 for file in listMH:
-    if file.endswith('.mp3')
+    if file.endswith('.mp3'):
         print(file)
 
 # Makes a 2D list of relevant track metadata
 os.chdir(MediaHuman)
 for file in listMH:
-    if file.endswith('.mp3')
-        # Load metadata each new file
+    if file.endswith('.mp3'):
+        # Load metadata from each new file
         audiofile = eyed3.load(file)
         artist = audiofile.tag.artist
         album = audiofile.tag.album
@@ -37,10 +38,16 @@ print(fulllist)
 for element in fulllist:
     os.chdir(Music)
 
-    # Checks for illegal characters in the metadata
-    ArtistSafe = element[0].replace('/', '-')
-    AlbumSafe = element[1].replace('/', '-')
-    ArtistSafe = ArtistSafe.replace('ö', 'o')
+    # Checks for illegal characters in the metadata, replaces missing data with "Unknown album/artist"
+    if namesafe(element[0]):
+        ArtistSafe = namesafe(element[0])
+    else:
+        ArtistSafe = 'Unknown Artist'
+    
+    if namesafe(element[1]):
+        AlbumSafe = namesafe(element[1])
+    else:
+        Albumsafe = 'Unknown Album'
 
     # Checks if the artist already has a legal designated folder and makes one if not
     if ArtistSafe not in listMU:
@@ -59,7 +66,7 @@ for element in fulllist:
     shutil.move(MediaHuman + '\\' + element[2], os.getcwd() + '\\' + element[2])                      
 
 
-# Leaves a timestamped log of moved media in the initial directory (Add date to timestamp)
+# Leaves a timestamped log of moved media in the initial directory
 os.chdir(MediaHuman)
 # Build and apply timestamp to log
 today = date.today()
@@ -70,3 +77,17 @@ f = open(current_time,"w+")
 f.write('List of moved tracks:')
 for element in fulllist:
     f.write(element[2] + ' (' + element[1] +')')
+
+
+
+def namesafe(argument):
+    illegal = r'<>\|?*:"/'
+    legallist = []
+    legal = ''
+    for character in argument:
+        if character not in illegal:
+            legallist.append(character)
+        else:
+            legallist.append('-')
+    legal = ''.join(legallist)
+    return(unidecode.unidecode_expect_ascii(legal))
